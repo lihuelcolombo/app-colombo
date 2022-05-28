61dbbbcdf983abd8883df48a7334100ddf0435a6
lihuelcolombo <lihuel.colombo@gmail.com>
2022-05-28 19:11:43 -0300

    agregado useParams para categoryId, no renderiza productos

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 import Item from './Item'
 
 
+// const ItemList = ( {items} ) => {
+//   return (
+//     <div className='pt-10 grid grid-cols-3 content-evenly'>
+//       {items.map( i => <Item key={i.id} data={i}/>)}
+//     </div>
+  
+//   )
+// }
+
 const ItemList = ( {items} ) => {
   return (
     <div className='pt-10 grid grid-cols-3 content-evenly'>
-      {items.map( i => <Item key={i.id} data={i}/>)}
+      {items & items.map((producto) => (
+        <Item key={producto.id} producto={producto}/>
+      ))}
     </div>
   
   )
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 import { getItems } from '../data/ItemList';
 import ItemList from './ItemList';
 
 const ItemListContainer = () => {
-  
-  
+    
   const [items, setItems] = useState([])
+  const { categoryId } = useParams()
 
   useEffect(() => {
-    getItems().then( data => {
-      setItems ( data )
-      console.log(data);
-    })
+    if (categoryId === undefined){
+      getItems().then((resp) => setItems (resp))
+    } else {
+      getItems().then((resp) => 
+        setItems(resp.filter((product) => product.category === categoryId ))
+      )
+    }
+  }, [categoryId])
+
+  //este useEffect funciona sin categorias
+  // useEffect(() => {
+  //   getItems().then( data => {
+  //     setItems ( data )
+  //     console.log(data);
+  //   })
     
-  }, [])
+  // }, [])
+
   
+  // esta promise fue movida hacia el ItemList.jsx
   // const getItems = () => { 
   //   return new Promise ( (resolve, reject) => {
   //     setTimeout(() => {
